449744d2cdd2219ebebe7382ae17be26799312b4
hasanbykkk <99807200+hasanbykkk@users.noreply.github.com>
2024-06-23 19:23:02 +0300

    kutleindeks

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textbox1 = new System.Windows.Forms.TextBox();
+            this.textbox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textbox1
+            // 
+            this.textbox1.Location = new System.Drawing.Point(60, 61);
+            this.textbox1.Multiline = true;
+            this.textbox1.Name = "textbox1";
+            this.textbox1.Size = new System.Drawing.Size(73, 43);
+            this.textbox1.TabIndex = 1;
+            // 
+            // textbox2
+            // 
+            this.textbox2.Location = new System.Drawing.Point(60, 123);
+            this.textbox2.Multiline = true;
+            this.textbox2.Name = "textbox2";
+            this.textbox2.Size = new System.Drawing.Size(73, 43);
+            this.textbox2.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(28, 34);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(65, 21);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "erkek";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(99, 34);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(64, 21);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "kadın";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "boy";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "kilo";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(182, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(606, 426);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textbox2);
+            this.Controls.Add(this.textbox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textbox1;
+        private System.Windows.Forms.TextBox textbox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = 4;
+
+            dataGridView1.Columns[0].Name = "cinsiyet";
+            dataGridView1.Columns[1].Name = "boy";
+            dataGridView1.Columns[2].Name = "kilo";
+            dataGridView1.Columns[3].Name = "vücut kitle indeksi";
+
+        }
+
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            int  kilo, boy;
+            kilo = Convert.ToInt32(textbox1.Text);
+            boy = Convert.ToInt32(textbox2.Text);
+
+            if (checkBox1.Checked) {
+
+                if (string.IsNullOrWhiteSpace(textbox1.Text))
+                {
+                    MessageBox.Show("boyunuzu girmediniz!!!");
+                }
+                else if (string.IsNullOrWhiteSpace(textbox2.Text))
+                {
+                    MessageBox.Show("kilonuzu girmediniz!!!");
+                }
+                else if (string.IsNullOrWhiteSpace(textbox1.Text) || string.IsNullOrWhiteSpace(textbox2.Text))
+                {
+                    MessageBox.Show("boy ve kilonuzu lütfen giriniz!!!");
+                }
+                else 
+                {
+                    int indeks = kilo / (boy * boy);
+                    dataGridView1.Rows.Add(checkBox1.Text,kilo, boy, indeks);
+                    textbox1.Clear();
+                    textbox2.Clear();
+                    checkBox1.Checked = false;
+                }
+
+            }
+
+
+
+            else if (checkBox2.Checked) { 
+
+                if (int.TryParse(textbox2.Text,out kilo))
+                {
+                    MessageBox.Show("boyunuzu girmediniz!!!");
+                }
+                else if (string.IsNullOrWhiteSpace(textbox2.Text))
+                {
+                    MessageBox.Show("kilonuzu girmediniz!!!");
+                }
+                else if (string.IsNullOrWhiteSpace(textbox1.Text)|| string.IsNullOrWhiteSpace(textbox2.Text))
+                {
+                    MessageBox.Show("boy ve kilonuzu lütfen giriniz!!!");
+                }
+                else 
+                {
+                    int indeks = kilo / (boy * boy);
+                    dataGridView1.Rows.Add(checkBox2.Text,kilo, boy, indeks);
+                    textbox1.Clear();
+                    textbox2.Clear();
+                    checkBox2.Checked = false;
+
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("cinsiyet seçiniz lütfen!!!");
+            }
+
+
+
+
+        }
+
+     
+    }
+}
